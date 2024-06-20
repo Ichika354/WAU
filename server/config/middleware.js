6ed93d72bdc9ab505c293008ff976cf53bed0e6b
Ichika354 <handa82624@gmail.com>
2024-06-20 14:18:30 +0700

    cors

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -6,7 +6,7 @@ const configureMiddleware = (app) => {
   app.use(express.json());
   app.use(
     cors({
-      origin: "*",
+      origin: ["http://127.0.0.1:5500","http://localhost:5173"],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
       optionsSuccessStatus: 200,
