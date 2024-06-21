4b0cbed067d11b67abb72434f4f34a036662d65c
Ichika354 <handa82624@gmail.com>
2024-06-21 13:50:22 +0700

    cors

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -4,9 +4,13 @@ import bodyParser from "body-parser";
 
 const configureMiddleware = (app) => {
   app.use(express.json());
+  app.use((req, res, next) => {
+    console.log("Request Origin:", req.headers.origin);
+    next();
+  });
   app.use(
     cors({
-      origin: ["http://127.0.0.1:5500","http://localhost:5173","https://ichika354.github.io/"],
+      origin: ["http://127.0.0.1:5500", "http://localhost:5173", "https://ichika354.github.io/pages"],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
       optionsSuccessStatus: 200,
@@ -14,7 +18,5 @@ const configureMiddleware = (app) => {
   );
   app.use(bodyParser.urlencoded({ extended: true }));
 };
-
-
 
 export default configureMiddleware;
